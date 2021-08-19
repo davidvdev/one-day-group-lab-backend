85b68fa05a6523b00494e7c2307a08918c432ecf
Anthony Lofredo <86216676+Lofredoa1@users.noreply.github.com>
2021-08-19 10:36:43 -0400

    Revert "connection.js - fixed err| /models - removed .gitkeep, added song.js"

--- db/connection.js
+++ db/connection.js
@@ -12,6 +12,6 @@ mongoose.connect(MONGODB_URI, config)
 mongoose.connection
     .on("open", () => console.log("connected to mongo"))
     .on("closed", () => console.log("disconnected to mongo"))
-    .on("error", (err) => console.log(err))
+    .on("error", (err) => console.log(error))
 
 module.exports = mongoose
